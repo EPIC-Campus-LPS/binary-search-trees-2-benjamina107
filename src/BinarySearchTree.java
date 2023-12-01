568d09edb90ff8fb14cea8bca222010c8ca1d0ff
benjamina107 <benjamina107@lpsk12.org>
2023-12-01 10:43:12 -0700

    ~

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,2 +1,43 @@
-public class BinarySearchTree {
+public class BinarySearchTree<E> {
+
+    private TreeNode<E> root;
+
+    private int numNodes;
+    private int height;
+
+    public void add(E value) {
+
+    }
+
+    public boolean contains(E value) {
+
+    }
+
+    public int countNodes() {
+
+    }
+
+    public int countLeafNodes() {
+
+    }
+
+    public int getHeight() {
+
+    }
+
+    public void printInorder() {
+
+    }
+
+    public void printPreorder() {
+
+    }
+
+    public void printPostorder() {
+
+    }
+
+    public E delete(E value) {
+
+    }
 }
