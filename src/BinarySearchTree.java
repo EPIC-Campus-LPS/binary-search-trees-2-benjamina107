3ce7e5d74a9058c1a2b8848d46c7d5b036546d7c
benjamina107 <benjamina107@lpsk12.org>
2023-12-01 11:25:42 -0700

    ~

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,16 +1,61 @@
-public class BinarySearchTree<E> {
+import com.sun.source.tree.Tree;
 
-    private TreeNode<E> root;
+public class BinarySearchTree {
 
-    private int numNodes;
-    private int height;
+    private TreeNode<Integer> root;
+    private int numNodes = 0;
 
-    public void add(E value) {
+    public void add(Integer value) {
 
+        TreeNode<Integer> val = new TreeNode<Integer>(value, null, null);
+
+        if (numNodes == 0) {
+           root = val;
+        } else {
+            TreeNode<Integer> current = root;
+
+            boolean loop = true;
+            while (loop) {
+
+                //tests if the value goes to the left or right of 'current'
+                if (value > current.getValue()) {
+
+                    //if the left child value is empty, set it as the new value
+                    if (current.getLeftChild() != null) {
+
+                        current.setLeftChild(val);
+                        loop = false;
+
+                    //if the left child value is occupied, make that the new 'current'
+                    } else {
+
+                        current = current.getLeftChild();
+
+                    }
+
+                } else {
+
+                    //if the right child value is empty, set it as the new value
+                    if (current.getRightChild() != null) {
+
+                        current.setRightChild(val);
+                        loop = false;
+
+                    //if the right child value is occupied, make that the new 'current'
+                    } else {
+
+                        current = current.getRightChild();
+
+                    }
+
+                }
+
+            }
+
+        }
     }
 
     public boolean contains(E value) {
-
     }
 
     public int countNodes() {
