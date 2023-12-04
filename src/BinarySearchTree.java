83bc3029587c76c68c66e492cee0d58c01fcd157
ben <142339150+benjamina107@users.noreply.github.com>
2023-12-04 16:22:23 -0700

    getheight

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -2,7 +2,7 @@ import com.sun.source.tree.Tree;
 
 public class BinarySearchTree {
 
-    private TreeNode<Integer> root;
+    private TreeNode<Integer> root =;
     private int numNodes = 0;
 
     public void add(Integer value) {
@@ -123,10 +123,90 @@ public class BinarySearchTree {
     }
 
     public int countLeafNodes() {
-        
+
+        if (numNodes = 0) {
+            return -1;
+        }
+
+        return countLeafNodesHelper(root)
+
+    }
+
+    public int countLeafNodesHelper(TreeNode<Integer> rt) {
+        //count of leaf nodes to be returned
+        int count = 0;
+
+        if(rt.getLeftChild == null && rt.getRightChild == null) {
+            // if you get to a leaf node, return with 1 to represent the 1 leaf node you just found
+            return 1;
+
+        }
+    
+        //if there's a child on the left
+        if(rt.getLeftChild != null) {
+
+            //add count result from next left node to this count
+            count += countLeafNodesHelper(rt.getLeftChild)
+
+        }
+
+        //if there's a child on the right
+        if(rt.getRightChild != null) {
+
+            //add count result from next right node to this count
+            count += countLeafNodesHelper(rt.getRightChild)
+
+        }
+
+        //return sum of descendant leaf nodes
+        return count;
+
     }
 
     public int getHeight() {
+        
+        if (numNodes = 0) {
+            return -1;
+        }
+
+        return heightHelper(root, 0);
+
+    }
+
+    public int heightHelper(TreeNode<Integer> rt, depth) {
+
+        //to compare which is the biggest, left or right
+        int leftDepth = -1;
+        int rightDepth = -1;
+
+        if(rt.getLeftChild == null && rt.getRightChild == null) {
+            //if it gets to the bottom, simply return the pre calculated depth
+            return depth;
+
+        }
+    
+
+        if(rt.getLeftChild != null) {
+
+            //goes down a level to left and adds 1 to depth
+            int leftDepth = heightHelper(rt.getLeftChild, depth + 1);
+
+        }
+
+    
+        if(rt.getRightChild != null) {
+
+            //goes down a level to right and adds 1 to depth
+            int rightDepth = heightHelper(rt.getRightChild, depth + 1);
+
+        }
+
+        //compare left and right heights to get the max
+        int biggestDepth = leftDepth;
+
+        if(leftDepth < rightDepth) {biggestDepth = rightDepth};
+
+        return biggestDepth;
 
     }
 
